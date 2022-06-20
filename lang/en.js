de1b1c74d4def3a3afaa16d5333d7b5525080eb3
Le Anh Tuan <anhtuanle.ka@gmail.com>
2022-06-20 11:38:05 +0700

    20/6: add i18n language for english and vietnamese

--- lang/en.js
+++ lang/en.js
@@ -1,3 +1,24 @@
 export default {
-    home: 'Home'
+    home: 'Home',
+    login: {
+        text: "Login",
+        welcome: 'Welcome to Microsoft Teams',
+        identity: {
+            placeholder: 'Email, username or phone number',
+            error: 'Please enter your email, username or phone number!'
+        },
+        password: {
+            placeholder: 'Password',
+            error: 'Please enter your password!'
+        },
+        remember: 'Remember me',
+        button: 'Sign in',
+        forgetPassword: 'Forgot your password?',
+        orSignIn: 'Or sign in with',
+        dontHaveAccount: "Don't have an account?",
+        signUpNow: 'Sign up now!'
+    },
+    signUp: {
+        text: 'Sign up'
+    }
 }
--- lang/vi.js
+++ lang/vi.js
@@ -1,3 +1,24 @@
 export default {
-    home: 'Trang chủ'
+    home: 'Trang chủ',
+    login: {
+        text: "Đăng nhập",
+        welcome: 'Chào mừng bạn đến với Microsoft Teams!',
+        identity: {
+            placeholder: 'Email, tên đăng nhập hoặc mật khẩu',
+            error: 'Hãy nhập thông tin đăng nhập của bạn!!'
+        },
+        password: {
+            placeholder: 'Mật khẩu',
+            error: 'Hãy nhập mật khẩu của bạn!'
+        },
+        remember: 'Nhớ thông tin',
+        button: 'Đăng nhập',
+        forgetPassword: 'Quên mật khẩu?',
+        orSignIn: 'Hoặc đăng nhập với',
+        dontHaveAccount: "Chưa có tài khoản?",
+        signUpNow: 'Đăng kí ngay!'
+    },
+    signUp: {
+        text: "Đăng kí"
+    }
 }
